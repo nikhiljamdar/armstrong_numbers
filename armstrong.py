3a9a9806f67cfdab3c4e2f75689d261ad9921b77
Nikhil Jamdar <100251717+nikhiljamdar@users.noreply.github.com>
2023-10-09 11:14:51 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+num = int(input("enter the number : "))
+sum = 0
+temp = num
+count = len(str(num))
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** count
+    temp //= 10
+
+if num == sum:
+    print(f"number {num} is armstrong number")
+
+else :
+    print(f"number {num} is not armstrong number")
